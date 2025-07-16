4cd7fe72628e26fbdff0348737975b34e814d2d6
Julian D <judadi1994@gmail.com>
2025-07-16 19:14:51 +0200

    Enhance Login Page with Animated Text and Style Updates
    - Added animation to the `Konnektaro` heading using `shadow-dance-text` class and refactored related layout.
    - Introduced `restartAnimation` function to trigger animation on click.
    - Updated global styles: text color adjustment and new `shadow-dance-text` class with keyframes for animation.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useEffect, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import {useRouter} from 'next/navigation'
 import {onAuthStateChanged, getIdToken, User} from 'firebase/auth'
 import {auth} from '@/utils/firebase'
@@ -10,6 +10,15 @@ import Head from 'next/head'
 export default function LoginPage() {
     const router = useRouter()
     const [loading, setLoading] = useState(true)
+    const textRef = useRef<HTMLHeadingElement>(null)
+
+    const restartAnimation = () => {
+        const el = textRef.current
+        if (!el) return
+        el.classList.remove('shadow-dance-text')
+        void el.offsetWidth
+        el.classList.add('shadow-dance-text')
+    }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, async (user: User | null) => {
@@ -40,19 +49,20 @@ export default function LoginPage() {
                 <title>Konnektaro Login</title>
             </Head>
             <div className="h-screen w-full flex flex-col justify-center items-center bg-login text-center px-4">
-                <h1 className="mb-12 font-bold text-4xl text-title italic">Konnektaro</h1>
-
-                <div className="w-full max-w-sm flex flex-col gap-4">
+                <div className="flex-grow flex items-center justify-center w-full">
+                    <h1  ref={textRef} onClick={restartAnimation} className="text-4xl font-bold shadow-dance-text">Konnektaro</h1>
+                </div>
+                <div className="w-full max-w-sm flex flex-col gap-4 mb-5">
                     <button
                         onClick={loginAnonymously}
-                        className="w-full py-4 button-primary rounded-md shadow-md  text-lg transition hover:bg-blue-600"
+                        className="w-full py-4 button-primary rounded-md shadow-md  text-lg transition"
                     >
                         Sign in as Guest
                     </button>
 
                     <button
                         onClick={loginWithGoogle}
-                        className="w-full py-4 button-secondary rounded-md shadow-md  text-lg transition hover:bg-green-600"
+                        className="w-full py-4 button-secondary rounded-md shadow-md  text-lg transition"
                     >
                         Sign in as Moderator
                     </button>
