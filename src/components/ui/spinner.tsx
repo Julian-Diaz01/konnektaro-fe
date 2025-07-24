14346690731a43ca9c1f99b64fee7896245503c5
Julian D <judadi1994@gmail.com>
2025-07-24 15:32:30 +0200

    Add `Spinner` component for loading states in `ui` directory

---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,8 @@
+export default function Spinner() {
+    return (
+        <div className="text-gray-600 text-lg flex flex-col items-center">
+        <div className="w-8 h-8 border-4 border-primary border-t-transparent rounded-full animate-spin mb-2" />
+            Loading...
+    </div>
+)
+}
