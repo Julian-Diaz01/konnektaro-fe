230f4b1db76fd758e6355e37ae9a373a05100c7d
Julian D <judadi1994@gmail.com>
2025-07-30 16:41:05 +0200

    Update hooks with JSDoc comments and refactor event routing
    - Added JSDoc comments to `useAuthUser` and `useOpenEvents` hooks for better type annotations and documentation.
    - Updated event routing to use `/edit-event` paths for event management.
    - Replaced event handling logic in `EventListAdmin` to align with new routing structure.

--- src/hooks/useAuthUser.ts
+++ src/hooks/useAuthUser.ts
@@ -4,6 +4,12 @@ import { useEffect, useState } from 'react'
 import { onAuthStateChanged, User } from 'firebase/auth'
 import { auth } from '@/utils/firebase'
 
+/** * Custom hook to manage Firebase authentication state.
+ * @returns {Object} An object containing the current user and loading state.
+ * - `user`: {User | null} The currently authenticated user or null if not authenticated.
+ * - `loading`: {boolean} True if the authentication state is still being determined, false*
+ * **/
+
 export default function useAuthUser() {
     const [user, setUser] = useState<User | null>(null)
     const [loading, setLoading] = useState(true)
