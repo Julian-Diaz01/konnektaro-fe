5b89f1f00f7647cabb4bde18d452afaf2a8b2935
Julian D <judadi1994@gmail.com>
2025-07-30 16:41:05 +0200

    Update hooks with JSDoc comments and refactor event routing
    - Added JSDoc comments to `useAuthUser` and `useOpenEvents` hooks for better type annotations and documentation.
    - Updated event routing to use `/edit-event` paths for event management.
    - Replaced event handling logic in `EventListAdmin` to align with new routing structure.

--- src/app/edit-event/page.tsx
+++ src/app/edit-event/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Suspense } from "react";
-import EventPageClient from "@/app/event/EventPageClient";
+import EventPageClient from "@/app/edit-event/EventPageClient";
 
 export default function EventPage() {
   return (
--- src/hooks/useOpenEvents.ts
+++ src/hooks/useOpenEvents.ts
@@ -3,7 +3,9 @@
 import { useEffect, useState } from 'react'
 import { getAllEvents } from '@/services/eventService'
 import { Event } from '@/types/models'
-
+/**
+ * This hook fetches all open events from the server and returns them along with a loading state.
+ * **/
 export default function useOpenEvents() {
     const [events, setEvents] = useState<Event[]>([])
     const [loading, setLoading] = useState(true)
