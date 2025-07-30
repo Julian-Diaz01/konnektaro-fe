bbfa23e46f29fdb3dc938f66c6141c2376ae7b0e
Julian D <judadi1994@gmail.com>
2025-07-30 11:26:04 +0200

    Set `output` property to `standalone` in `next.config.ts` for standalone build configuration.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     //output: 'export', // enables `next export`
+    output: 'standalone',
     images: {
         unoptimized: true // required for static hosting if using <Image />
     }
