b632f2c0f099f1b7026eedf39ebcde25f0f1e4ca
Julian D <judadi1994@gmail.com>
2025-08-29 21:55:37 +0200

    Refactor postbuild script and Firebase configuration
    - Replace inline `postbuild` command in `package.json` with `cleanup.bat` for improved script management.
    - Remove `trailingSlash` from `next.config.ts`.
    - Simplify `firebase.json` by removing unnecessary headers and exclusions.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     output: 'export', // enables `next export` for Firebase Hosting
-    trailingSlash: true,
     images: {
         unoptimized: true // required for static hosting if using <Image />
     },
