1f03fffcf6ac4ecb4110e464e8521ff0be3ca964
Julian D <judadi1994@gmail.com>
2025-08-29 20:01:59 +0200

    Update Next.js config and Firebase headers for static hosting compatibility
    - Enable `trailingSlash` in `next.config.ts` for proper routing.
    - Add `Content-Type` headers for `.html` files in `firebase.json`.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     output: 'export', // enables `next export` for Firebase Hosting
+    trailingSlash: true,
     images: {
         unoptimized: true // required for static hosting if using <Image />
-    }
+    },
 };
 
 export default nextConfig;
