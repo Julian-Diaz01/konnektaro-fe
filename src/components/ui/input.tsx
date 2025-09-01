375fcac524012a31480b782cf0e4cd674a5fc435
Julian D <judadi1994@gmail.com>
2025-09-01 14:10:28 +0200

    Refactor `textarea` and `input` components for improved styling and consistency
    - Update `textarea` label class for better alignment and readability.
    - Revise `textarea` and `input` styles to unify design, enhance accessibility, and ensure responsiveness.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,8 +9,8 @@ function Input({ className, type, maxLength, ...props }: React.ComponentProps<"i
       data-slot="input"
       maxLength={maxLength}
       className={cn(
-          "text-black file:text-foreground placeholder:text-muted-foreground dark:bg-white border-input flex h-9 w-full min-w-0 rounded-md border bg-white px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-white file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "text-black file:text-foreground placeholder:text-muted-foreground dark:bg-white border-input flex h-9 w-full min-w-0 rounded-md border bg-white px-3 py-1 shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-white file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-ring focus-visible:ring-[var(--primary)] focus-visible:ring-[2px]",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
           "selection:bg-[var(--primary-60)] selection:text-black",
           className
